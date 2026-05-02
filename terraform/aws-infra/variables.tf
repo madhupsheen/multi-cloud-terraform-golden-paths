428313a227cd2d333a3448b2f56636458dab80c4
Madhup Sheen <56523918+madhupsheen@users.noreply.github.com>
2026-05-02 18:48:21 +1200

    Update variables.tf

--- terraform/aws-infra/variables.tf
+++ terraform/aws-infra/variables.tf
@@ -1,12 +1,25 @@
 variable "region" {
-  default = "ap-southeast-2" # Sydney
+  description = "The AWS region to deploy the platform foundations"
+  type        = string
+  default     = "ap-southeast-2" 
+}
+
+variable "environment_name" {
+  description = "The name of the environment or organization"
+  type        = string
+  default     = "platform-foundation"
 }
 
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.region
 }
